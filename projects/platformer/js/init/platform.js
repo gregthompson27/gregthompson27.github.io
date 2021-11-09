4d18378d84b84dd81b14ce87b9be24bfbb31a3d6
Gregory Thompson <gregthompson27@gmail.com>
2021-11-09 14:48:28 +0000

    add index.html file

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,9 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
-        createPlatform(170, 460, 0.5);
+        // createPlatform(400, 250);
+        // createPlatform(170, 460, 0.5);
+        createPlatform(310, 580, 0.75);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
