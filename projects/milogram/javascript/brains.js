0bac150e75069af0f9990cf061f4a465a2ce88ec
Greg <gregthompson27@gmail.com>
2022-03-11 11:11:35 -0600

    add milogram changes

--- projects/milogram/javascript/brains.js
+++ projects/milogram/javascript/brains.js
@@ -21,24 +21,24 @@ const user = {
 function likePhoto(num) {
     if (num === 1) {
         counter1++;
-        document.getElementById('likeCount1').innerHTML = counter1;
+        document.getElementById("likeCount1").innerHTML = counter1;
     } else if (num === 2) {
         counter2++;
-        document.getElementById('likeCount2').innerHTML = counter2;
+        document.getElementById("likeCount2").innerHTML = counter2;
     } else if (num === 3) {
         counter3++;
-        document.getElementById('likeCount3').innerHTML = counter3;
+        document.getElementById("likeCount3").innerHTML = counter3;
     } else if (num === 4) {
         counter4++;
-        document.getElementById('likeCount4').innerHTML = counter4;
+        document.getElementById("likeCount4").innerHTML = counter4;
     } else if (num === 5) {
         counter5++;
-        document.getElementById('likeCount5').innerHTML = counter5;
+        document.getElementById("likeCount5").innerHTML = counter5;
     }
 }
 
 // debugger;
-​setTimeout(function(){ document.getElementById("first-name").innerHTML = user.firstName;}, 3000);
-​setTimeout(function(){ document.getElementById("last-name").innerHTML = user.lastName;}, 3000);
-​setTimeout(function(){ document.getElementById("followers").innerHTML = "Followers: " + user.followers.length;}, 3000);
-​setTimeout(function(){ $("#profile-pic").attr("src", user.profilePic);}, 3000);
+// ​setTimeout(function(){ document.getElementById("first-name").innerHTML = user.firstName;}, 3000);
+// ​setTimeout(function(){ document.getElementById("last-name").innerHTML = user.lastName;}, 3000);
+// ​setTimeout(function(){ document.getElementById("followers").innerHTML = "Followers: " + user.followers.length;}, 3000);
+// ​setTimeout(function(){ $("#profile-pic").attr("src", user.profilePic);}, 3000);
