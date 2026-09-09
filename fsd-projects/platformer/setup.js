48a2f0d5afc99c702996fa294f7ae31cc8af39f0
gregthompson27 <gregthompson27@gmail.com>
2026-09-09 10:53:24 -0500

    add console.log statement to platformer setup file

--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -1,3 +1,4 @@
+console.log('setting up platformer.js...');
 // setup variables
 const walkAcceleration = 2.5; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
